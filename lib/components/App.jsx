db0366e1ff06d92f69a449a2ffeebcfa6539866a
gness1804 <graham.nessler@gmail.com>
2016-10-16 16:32:02 -0600

    looks like data is being passed in

--- lib/components/App.jsx
+++ lib/components/App.jsx
@@ -23,7 +23,8 @@ class App extends React.Component {
     let city = this.state.location.toUpperCase();
     $.get("http://api.wunderground.com/api/47fe8304fc0c9639/forecast/q/CA/San_Francisco.json", function (data) {
       that.setState({weather:data});
-      console.log(data.forecast.txt_forecast.date);
+      // console.log(data.forecast.txt_forecast.date);
+      // console.log(that.state.weather);
     });
     // if (city === "DENVER") {
     //   $.get("http://weatherly-api.herokuapp.com/api/weather/denver", function (data) {
@@ -102,57 +103,61 @@ class WeatherList extends React.Component {
     super(props);
   }
 
-  showWeatherData(data) {
-
-    return(<div className="weather-card">
-
-        // <p className='date'>{data.date}</p>
-        // <p className="weather-card-text1"> The weather will be {data.weatherType.type} with a high of {data.temp.high} and a low of {data.temp.low}.</p>
-        // <p className="weather-card-text2">There will be a {Math.floor(data.weatherType.chance * 100)} percent chance of this weather event happening.
-        <p>Hi</p>
-      </p>
-    </div>
-    );
-  } //end of showWeatherData
-
-  showExtremeWeather(data) {
-    if (data.weatherType.scale === 3) {
-      if (data.weatherType.type === "sunny") {
-        return(<div className="warning sun">
-        <p className='date'>{data.date}</p><p className="weather-card-text1">There will be extreme sun. </p>
-          <p className="weather-card-text2">Take care and use plenty of sunscreen!
-          </p>
-        </div>);
-      }
-      else if (data.weatherType.type === "rain") {
-        return(<div className='warning rain'>
-        <p className='date'>{data.date}</p><p className="weather-card-text1">There will be a high chance of flooding and extreme rain. </p>
-          <p className="weather-card-text2">Stay inside and don't drive if possible!
-          </p>
-        </div>);
-      }
-      else if (data.weatherType.type === "windy") {
-        return(<div className='warning windy'>
-          <p className='date'>{data.date}</p> <p className="weather-card-text1">There will be very high winds.</p>
-          <p className="weather-card-text2">Stay indoors!</p>
-        </div>);
-      }
-      else if (data.weatherType.type === "snow") {
-        return(<div className='warning snow'>
-        <p className='date'>{data.date}</p><p className="weather-card-text1">There will be heavy snow!
-          </p>
-          <p className="weather-card-text2">Take precautions!
-          </p>
-        </div>);
-      }
-
-    }
+  logData(){
+    console.log(this.props.data);
   }
+
+  // showWeatherData(data) {
+  //
+  //   return(<div className="weather-card">
+  //
+  //       // <p className='date'>{data.date}</p>
+  //       // <p className="weather-card-text1"> The weather will be {data.weatherType.type} with a high of {data.temp.high} and a low of {data.temp.low}.</p>
+  //       // <p className="weather-card-text2">There will be a {Math.floor(data.weatherType.chance * 100)} percent chance of this weather event happening.
+  //       <p>Hi</p>
+  //     </p>
+  //   </div>
+  //   );
+  // } //end of showWeatherData
+  //
+  // showExtremeWeather(data) {
+  //   if (data.weatherType.scale === 3) {
+  //     if (data.weatherType.type === "sunny") {
+  //       return(<div className="warning sun">
+  //       <p className='date'>{data.date}</p><p className="weather-card-text1">There will be extreme sun. </p>
+  //         <p className="weather-card-text2">Take care and use plenty of sunscreen!
+  //         </p>
+  //       </div>);
+  //     }
+  //     else if (data.weatherType.type === "rain") {
+  //       return(<div className='warning rain'>
+  //       <p className='date'>{data.date}</p><p className="weather-card-text1">There will be a high chance of flooding and extreme rain. </p>
+  //         <p className="weather-card-text2">Stay inside and don't drive if possible!
+  //         </p>
+  //       </div>);
+  //     }
+  //     else if (data.weatherType.type === "windy") {
+  //       return(<div className='warning windy'>
+  //         <p className='date'>{data.date}</p> <p className="weather-card-text1">There will be very high winds.</p>
+  //         <p className="weather-card-text2">Stay indoors!</p>
+  //       </div>);
+  //     }
+  //     else if (data.weatherType.type === "snow") {
+  //       return(<div className='warning snow'>
+  //       <p className='date'>{data.date}</p><p className="weather-card-text1">There will be heavy snow!
+  //         </p>
+  //         <p className="weather-card-text2">Take precautions!
+  //         </p>
+  //       </div>);
+  //     }
+  //
+  //   }
+  // } //end of showExtremeWeather
 
   render () {
     return (
       <ul>
-        <li>Hi</li>
+        <li>{this.logData()}</li>
       </ul>
     );
   }
