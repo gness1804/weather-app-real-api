63c6147f987e79941767a265f43b8e36e43f7402
gness1804 <graham.nessler@gmail.com>
2016-10-16 20:07:25 -0600

    deleted unneeded jsx file; got mvp

--- lib/components/App.jsx
+++ lib/components/App.jsx
@@ -1,7 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const WeatherButton = require('./WeatherButton.jsx');
-// require('./LocalStorage.jsx');
 
 class App extends React.Component {
   constructor(props) {
